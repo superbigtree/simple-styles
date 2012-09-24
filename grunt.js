20db9b2067cdf311eba1979dec34c62acea11f73
sethvincent <sethvincent@gmail.com>
2012-09-23 20:26:50 -0700

    get started with stylus

--- grunt.js
+++ grunt.js
@@ -1,8 +1,21 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    stylus: {
+      compile: {
+        options: {
+          compress: true,
+          paths: ['styles']
+        },
+        files: {
+          'exp.css': 'styles/main.styl'
+        }
+      }
+    }
   
   });
   
-  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  
+  grunt.registerTask('default', 'stylus');
 };
