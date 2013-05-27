14f803b76875f897d59201a962f93a9529c06f4f
sethvincent <sethvincent@gmail.com>
2013-05-27 00:23:08 -0700

    v0.0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,11 @@
+'use strict';
+var path = require('path');
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+var folderMount = function folderMount(connect, point) {
+  return connect.static(path.resolve(point));
+};
+
 module.exports = function(grunt) {
   grunt.initConfig({    
     pkg: grunt.file.readJSON('package.json'),
@@ -12,17 +20,40 @@ module.exports = function(grunt) {
         }
       }
     },
-    
-    mincss: {
-      compress: {
+
+    cssmin: {
+      combine: {
         files: {
           'simple-styles.min.css': 'simple-styles.css'
         }
+      }
+    },
+
+    connect: {
+      livereload: {
+        options: {
+          port: 9002,
+          middleware: function(connect, options) {
+            return [lrSnippet, folderMount(connect, '.')]
+          }
+        }
+      }
+    },
+
+    // Configuration to be run (and then tested)
+    regarde: {
+      project: {
+        files: ['*.js', '*.css', '*.html'],
+        tasks: ['livereload']
       }
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-mincss');
-  grunt.registerTask('default', ['sass', 'mincss']);
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+
+  grunt.registerTask('default', ['sass', 'cssmin', 'livereload-start', 'connect', 'regarde']);
 };
